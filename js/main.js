8f3755018d4e332515ec139ddd4e45a50fed7212
The-Code-Artist <code_artist@outlook.com>
2020-01-28 18:42:07 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,278 @@
+(function ($) {
+    "use strict";
+
+    $("body").scrollspy({ target: ".navbar-collapse", offset: 200 });
+
+    // SLIDER REVOLUTION
+    jQuery('.tp-banner').show().revolution({
+        dottedOverlay: "none",
+        delay: 6000,
+        startwidth: 1170,
+        startheight: 605,
+        hideThumbs: 200,
+
+        thumbWidth: 100,
+        thumbHeight: 50,
+        thumbAmount: 5,
+
+        navigationType: "bullet",
+        navigationArrows: "solo",
+        navigationStyle: "preview1",
+
+        touchenabled: "on",
+        onHoverStop: "on",
+
+        swipe_velocity: 0.7,
+        swipe_min_touches: 1,
+        swipe_max_touches: 1,
+        drag_block_vertical: false,
+
+        parallax: "mouse",
+        parallaxBgFreeze: "on",
+        parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
+
+        keyboardNavigation: "on",
+
+        navigationHAlign: "center",
+        navigationVAlign: "bottom",
+        navigationHOffset: 0,
+        navigationVOffset: 0,
+
+        soloArrowLeftHalign: "left",
+        soloArrowLeftValign: "center",
+        soloArrowLeftHOffset: 20,
+        soloArrowLeftVOffset: 0,
+
+        soloArrowRightHalign: "right",
+        soloArrowRightValign: "center",
+        soloArrowRightHOffset: 20,
+        soloArrowRightVOffset: 0,
+
+        shadow: 1,
+        fullWidth: "on",
+        fullScreen: "on",
+
+        spinner: "spinner4",
+
+        stopLoop: "off",
+        stopAfterLoops: -1,
+        stopAtSlide: -1,
+
+        shuffle: "off",
+
+        autoHeight: "off",
+        forceFullWidth: "off",
+
+        hideThumbsOnMobile: "off",
+        hideNavDelayOnMobile: 1500,
+        hideBulletsOnMobile: "off",
+        hideArrowsOnMobile: "off",
+        hideThumbsUnderResolution: 0,
+
+        hideSliderAtLimit: 0,
+        hideCaptionAtLimit: 0,
+        hideAllCaptionAtLilmit: 0,
+        startWithSlide: 0,
+        videoJsPath: "rs-plugin/videojs/",
+        fullScreenOffsetContainer: ""
+    });
+
+    // SLIDER REVOLUTION
+    jQuery('.tp-banner1').show().revolution({
+        dottedOverlay: "none",
+        delay: 16000,
+        startwidth: 1170,
+        startheight: 550,
+        hideThumbs: 200,
+
+        thumbWidth: 100,
+        thumbHeight: 50,
+        thumbAmount: 5,
+
+        navigationType: "bullet",
+        navigationArrows: "solo",
+        navigationStyle: "preview5",
+
+        touchenabled: "on",
+        onHoverStop: "on",
+
+        swipe_velocity: 0.7,
+        swipe_min_touches: 1,
+        swipe_max_touches: 1,
+        drag_block_vertical: false,
+
+        parallax: "mouse",
+        parallaxBgFreeze: "on",
+        parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
+
+        keyboardNavigation: "off",
+
+        navigationHAlign: "center",
+        navigationVAlign: "bottom",
+        navigationHOffset: 0,
+        navigationVOffset: 20,
+
+        soloArrowLeftHalign: "left",
+        soloArrowLeftValign: "center",
+        soloArrowLeftHOffset: 20,
+        soloArrowLeftVOffset: 0,
+
+        soloArrowRightHalign: "right",
+        soloArrowRightValign: "center",
+        soloArrowRightHOffset: 20,
+        soloArrowRightVOffset: 0,
+
+        shadow: 0,
+        fullWidth: "on",
+        fullScreen: "off",
+
+        spinner: "spinner4",
+
+        stopLoop: "off",
+        stopAfterLoops: -1,
+        stopAtSlide: -1,
+
+        shuffle: "off",
+
+        autoHeight: "off",
+        forceFullWidth: "off",
+
+        hideThumbsOnMobile: "off",
+        hideNavDelayOnMobile: 1500,
+        hideBulletsOnMobile: "off",
+        hideArrowsOnMobile: "off",
+        hideThumbsUnderResolution: 0,
+
+        hideSliderAtLimit: 0,
+        hideCaptionAtLimit: 0,
+        hideAllCaptionAtLilmit: 0,
+        startWithSlide: 0,
+        videoJsPath: "rs-plugin/videojs/",
+        fullScreenOffsetContainer: ""
+    });
+
+    // SLIDER REVOLUTION
+    jQuery('.tp-banner-full').show().revolution({
+        dottedOverlay: "none",
+        delay: 16000,
+        startwidth: 1170,
+        startheight: 700,
+        hideThumbs: 200,
+
+        thumbWidth: 100,
+        thumbHeight: 50,
+        thumbAmount: 5,
+
+        navigationType: "bullet",
+        navigationArrows: "solo",
+        navigationStyle: "preview5",
+
+        touchenabled: "on",
+        onHoverStop: "on",
+
+        swipe_velocity: 0.7,
+        swipe_min_touches: 1,
+        swipe_max_touches: 1,
+        drag_block_vertical: false,
+
+        parallax: "mouse",
+        parallaxBgFreeze: "on",
+        parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
+
+        keyboardNavigation: "off",
+
+        navigationHAlign: "center",
+        navigationVAlign: "bottom",
+        navigationHOffset: 0,
+        navigationVOffset: 20,
+
+        soloArrowLeftHalign: "left",
+        soloArrowLeftValign: "center",
+        soloArrowLeftHOffset: 20,
+        soloArrowLeftVOffset: 0,
+
+        soloArrowRightHalign: "right",
+        soloArrowRightValign: "center",
+        soloArrowRightHOffset: 20,
+        soloArrowRightVOffset: 0,
+
+        shadow: 0,
+        fullWidth: "on",
+        fullScreen: "on",
+
+        spinner: "spinner4",
+
+        stopLoop: "off",
+        stopAfterLoops: -1,
+        stopAtSlide: -1,
+
+        shuffle: "off",
+
+        autoHeight: "off",
+        forceFullWidth: "off",
+
+        hideThumbsOnMobile: "off",
+        hideNavDelayOnMobile: 1500,
+        hideBulletsOnMobile: "off",
+        hideArrowsOnMobile: "off",
+        hideThumbsUnderResolution: 0,
+
+        hideSliderAtLimit: 0,
+        hideCaptionAtLimit: 0,
+        hideAllCaptionAtLilmit: 0,
+        startWithSlide: 0,
+        videoJsPath: "rs-plugin/videojs/",
+        fullScreenOffsetContainer: ""
+    });
+
+    $('.b-progress-bar').each(function () {
+        var cap = parseInt($(this).attr('data-capacity'), 10),
+            val = parseInt($(this).attr('data-value'), 10),
+            len = 100 * (val / cap) + '%';
+
+        $(this).find('.progress-line').css('width', len);
+
+    });
+
+    // Skills 
+    $('#skills').appear(function () {
+        $(".progress-scale div").removeClass("no-width-skills");
+    }, {
+        accX: 0,
+        accY: -200
+    });
+
+
+    $('#skills2').appear(function () {
+        $('.chart').easyPieChart({
+            barColor: '#000000'
+        });
+
+        $('.chart-color2').easyPieChart({
+            barColor: '#7cc623'
+        });
+
+        $('.chart-color3').easyPieChart({
+            barColor: '#0FA2D5'
+        });
+
+        $('.chart-color4').easyPieChart({
+            barColor: '#FF4862'
+        });
+
+    }, {
+        accX: 0,
+        accY: -200
+    });    
+})(jQuery);
+// End of use strict
+
+//preloader /
+$(document).ready(function () {
+
+    // preloader
+    $(window).load(function () {
+        $('.preloader').delay(400).fadeOut(500);
+    })
+
+})
